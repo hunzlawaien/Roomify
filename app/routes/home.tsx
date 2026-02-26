e0820b342d2009d8b910226bb23943a985c2764a
Hunzla <hunzlawaien@gmail.com>
2026-02-26 15:08:33 +0500

    fixed bugs

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             console.error("Failed to create a project");
             return false;
         }
-        setProjects((prev) =>[newItem, ...prev]);
+        setProjects((prev) =>[saved, ...prev]);
 
         navigate(`/visualizer/${newId}`,{
             state: {
@@ -90,7 +90,7 @@ export default function Home() {
                     </div>
                     <div className="projects-grid">
                         {projects.map(({id,name,renderedImage,sourceImage,timestamp}) => (
-                            <div className="project-card group">
+                            <div key={id} className="project-card group">
                                 <div className="preview">
                                     <img src={renderedImage || sourceImage} alt="Project Preview"/>
                                     <div className="badge">
